11dbe9afe5e7cc4c5c3af6d4a3b51a1aba6d5a1c
yiyedata <49472092+yiyedata@users.noreply.github.com>
2019-08-14 20:43:56 +0800

    add RedisUrlStore

--- spiders/demoSpider.py
+++ spiders/demoSpider.py
@@ -3,12 +3,16 @@
 import json
 from core.logPrint import logPrint,logError,getTime,appendFile
 from core.spider import Spider 
+from redis_urlstore import RedisUrlStore
 class DemoSpider(Spider):
   # concurrencyPer1s=2
   name = 'demo-spider'
   start_urls = ['http://health.sina.com.cn/']
   models = ['auto_main_2','auto_obj']
 
+  # Storing URLs with redis, if you don't like this, please comment it out 
+  url_store = RedisUrlStore()
+  
   def afterResponse(self, response, cookie, url):
     html = Spider.afterResponse(self,response,cookie,url)
     return Spider.removeScripts(self,html)
