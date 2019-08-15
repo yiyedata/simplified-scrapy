6bb34ef342828b698d2f47a174c51428a82a4c5d
yiyedata <49472092+yiyedata@users.noreply.github.com>
2019-08-15 08:57:42 +0800

    Update demoSpider.py

--- spiders/demoSpider.py
+++ spiders/demoSpider.py
@@ -1,9 +1,6 @@
-#!/usr/bin/python
-#coding=utf-8
-import json
-from core.logPrint import logPrint,logError,getTime,appendFile
 from core.spider import Spider 
 from redis_urlstore import RedisUrlStore
+from mongo_objstore import MongoObjStore
 class DemoSpider(Spider):
   # concurrencyPer1s=2
   name = 'demo-spider'
@@ -12,18 +9,9 @@ class DemoSpider(Spider):
 
   # Storing URLs with redis, if you don't like this, please comment it out 
   url_store = RedisUrlStore()
-  
+  # Storing Objs with mongodb, if you don't like this, please comment it out 
+  obj_store = MongoObjStore()
+
   def afterResponse(self, response, cookie, url):
     html = Spider.afterResponse(self,response,cookie,url)
     return Spider.removeScripts(self,html)
-
-  def saveObj(self, data):
-    d = data.get("Data")
-    if(d and len(d) > 0):
-      objs=d[0].get("Datas")
-      if(objs and len(objs)>0):
-        appendFile('obj.json', json.dumps(objs))
-    
-
-  # def saveUrl(self, data):
-  #   saveFile('url.json', data)
