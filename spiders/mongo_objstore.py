364dcc2700aff4a3f2df621a894012e09f79eb4b
yiyedata <49472092+yiyedata@users.noreply.github.com>
2019-08-15 08:53:15 +0800

    Add files via upload

---
+++ spiders/mongo_objstore.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+#coding=utf-8
+from pymongo import MongoClient
+class MongoObjStore:
+  _host='127.0.0.1'
+  _port=27017
+  _dbName='python_db'
+  _tbName='obj_tb'
+  def _connect(self):
+    conn = MongoClient(self._host, self._port)
+    return conn[self._dbName]
+
+  def saveObj(self, data):
+    objs=data.get("Datas")
+    if(objs and len(objs)>0):
+      db=self._connect()
+      db[self._tbName].insert(data)
