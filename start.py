a11745f5649492e26452ffe937eb09c07e5bb108
yiyedata <3095069599@qq.com>
2019-12-07 18:37:59 +0800

    0.2.35

--- start.py
+++ start.py
@@ -1,17 +1,6 @@
 #!/usr/bin/python
 #coding=utf-8
 from simplified_scrapy.simplified_main import SimplifiedMain
-
-thrc = SimplifiedMain()
-thrc.setRunFlag(True)
-thrc.startThread()
+SimplifiedMain.startThread()
 
 
-#www.sohu.com
-# myint=0
-# for c in 'www.sohu.com':
-#   myint += ord(c)
-# print myint % 10
-
-# test = {}
-# print test.get('tets')
