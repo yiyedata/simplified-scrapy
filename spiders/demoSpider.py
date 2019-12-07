82c57834b399d203df69fdad010e961475641672
yiyedata <3095069599@qq.com>
2019-12-07 18:41:23 +0800

    0.2.35

--- spiders/demoSpider.py
+++ spiders/demoSpider.py
@@ -1,12 +1,13 @@
-from core.spider import Spider 
-from core.redis_urlstore import RedisUrlStore
-from core.mongo_objstore import MongoObjStore
+from simplified_scrapy.spider import Spider 
 class DemoSpider(Spider):
 
   name = 'demo-spider'
   start_urls = ['http://www.scrapyd.cn/']
   models = ['auto_main_2','auto_obj']
 
+  
+  # from simplified_scrapy.core.redis_urlstore import RedisUrlStore
+  # from simplified_scrapy.core.mongo_objstore import MongoObjStore
   # Storing URLs with redis, if you don't like this, please comment it out 
   # url_store = RedisUrlStore(name,{'host':'127.0.0.1','port':6379})
   # Storing Objs with mongodb, if you don't like this, please comment it out 
@@ -15,3 +16,5 @@ class DemoSpider(Spider):
   def afterResponse(self, response, url):
     html = Spider.afterResponse(self, response, url)
     return Spider.removeScripts(self, html)
+from simplified_scrapy.simplified_main import SimplifiedMain
+SimplifiedMain.startThread(DemoSpider())
